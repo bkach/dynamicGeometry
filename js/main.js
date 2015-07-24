3fc40002d490633bb552c3722e4828a762a3993d
Boris Kachscovsky <kachscovsky@gmail.com>
2015-07-24 20:18:57 +0200

    non running implementation works

--- js/main.js
+++ js/main.js
@@ -4,14 +4,14 @@ var scene = new THREE.Scene();
 // Camera
 var camera = new THREE.PerspectiveCamera(45, 
     window.innerWidth / window.innerHeight, 1, 10000);
-camera.position.z = 70;
 scene.add(camera);
 
 var axisHelper = new THREE.AxisHelper( 30 );
 scene.add( axisHelper );
 
 // Controls
-var controls = new THREE.TrackballControls(camera);
+// var controls = new THREE.TrackballControls(camera);
+// controls.target.x += camera.position.x;
 
 // Renderer
 var renderer = new THREE.WebGLRenderer({
@@ -23,69 +23,77 @@ document.body.appendChild(renderer.domElement);
 
 MAX_POINTS = 10000;
 var startingPoint = new THREE.Vector3(0,0,0);
-var startingFace = new THREE.Face3(0,1,2);
+var startingFace = new THREE.Face3(0,0,0);
 var geo = new THREE.Geometry();
 for(i=0; i < MAX_POINTS; i++){
   geo.vertices.push(startingPoint.clone());
   geo.faces.push(startingFace.clone());
 }
 
-//bottom
-//First row
-
-geo.vertices[0] = new THREE.Vector3( -30,  0,  0 );
-geo.vertices[1] = new THREE.Vector3( -20,  0,  0 );
-geo.vertices[2] = new THREE.Vector3( -10,  0,  0 );
-geo.vertices[3] = new THREE.Vector3(   0,  0,  0  );
-geo.vertices[4] = new THREE.Vector3(  10,  0,  0 );
-geo.vertices[5] = new THREE.Vector3(  20,  0,  0 );
-geo.vertices[6] = new THREE.Vector3(  30,  0,  0 );
-
 var mesh = new THREE.Mesh(
     geo,
     new THREE.MeshNormalMaterial()
     );
 
+mesh.position.y += 10
+mesh.frustumCulled = false;
+
+mesh.geometry.computeFaceNormals();
+mesh.geometry.computeVertexNormals();
+
 scene.add(mesh);
 
 window.addEventListener("click",onClick);
 
-var numLevels = 1;
-var boxesPerRow = 6;
-var boxSize = 10;
-var leftBoundary = -1 * (boxSize * boxesPerRow / 2);
+var currentRow = 0;
+var rows = 150;
+var size = 10;
+
+camera.position.x = (rows * size) / 2;
+camera.position.z = (rows * size) / 2;
+
 function onClick(e){
-  console.log("click");
-  // Create points
-  for(var i=0; i < boxesPerRow; i++){
-    mesh.geometry.vertices[(boxesPerRow+1) * numLevels + i] = new THREE.Vector3(
-        leftBoundary + (boxSize*i),
-        numLevels*boxSize,
-        0);
+  if(currentRow == 0){
+    // create initial points - c for column
+    for(var c=0; c < rows; c++){
+      geo.vertices[c] = new THREE.Vector3(c * size, 0, 0);
+    }
+    currentRow++;
   }
-  // Create faces
-  for(var i=0; i < boxesPerRow; i++){
-    mesh.geometry.faces[2*boxesPerRow*numLevels+(2*i)] = new THREE.Face3(
-        (numLevels*(boxesPerRow+1)) + i,
-        ((numLevels-1) * (boxesPerRow+1) + i),
-        (numLevels*(boxesPerRow+1)) + i + 1
+  else{
+    // Create points
+    for(var c=0; c < rows+1; c++){
+      mesh.geometry.vertices[currentRow * (rows+1) + c] = new THREE.Vector3(
+          size*c,
+          currentRow*size,
+          Math.random() * 40);
+    }
+    // Create faces
+    for(var c=0; c < rows; c++){
+      mesh.geometry.faces[2*rows*currentRow+(2*c)] = new THREE.Face3(
+          (currentRow*(rows+1)) + c,
+          ((currentRow-1) * (rows+1) + c),
+          (currentRow*(rows+1)) + c + 1);
+      mesh.geometry.faces[2*rows*currentRow+(2*c)+1] = new THREE.Face3(
+          ((currentRow-1) * (rows+1) + c), //0
+          ((currentRow-1) * (rows+1) + c + 1), //1
+          (currentRow*(rows+1)) + c + 1 //8
         );
-    mesh.geometry.faces[2*boxesPerRow*numLevels+(2*i)+1] = new THREE.Face3(
-        ((numLevels-1) * (boxesPerRow+1) + i), //0
-        ((numLevels-1) * (boxesPerRow+1) + i + 1), //1
-        (numLevels*(boxesPerRow+1)) + i + 1 //8
-      );
+    }
+    mesh.geometry.verticesNeedUpdate = true;
+    mesh.geometry.elementsNeedUpdate = true;
+    mesh.geometry.normalsNeedUpdate = true;
+    mesh.geometry.computeFaceNormals();
+    mesh.geometry.computeVertexNormals();
+    currentRow++;
+
+    mesh.position.y -= 10;
   }
-  mesh.geometry.computeFaceNormals();
-  mesh.geometry.computeVertexNormals();
-  mesh.geometry.verticesNeedUpdate = true;
-  mesh.geometry.elementsNeedUpdate = true;
-  numLevels++;
 }
 
 // Render and animate
 var render = function(){
-    controls.update();
+    // controls.update();
     renderer.render(scene,camera);
 }
 
