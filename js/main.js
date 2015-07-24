70f1e1aca0c137c52716a822b8e34a46f9e412d8
Boris Kachscovsky <kachscovsky@gmail.com>
2015-07-24 21:52:36 +0200

    got z axis working automatically

--- js/main.js
+++ js/main.js
@@ -4,14 +4,16 @@ var scene = new THREE.Scene();
 // Camera
 var camera = new THREE.PerspectiveCamera(45, 
     window.innerWidth / window.innerHeight, 1, 10000);
+camera.position.set(0,0,50);
 scene.add(camera);
+
+// Controls
+var controls = new THREE.TrackballControls(camera);
+console.log(controls);
 
 var axisHelper = new THREE.AxisHelper( 30 );
 scene.add( axisHelper );
 
-// Controls
-// var controls = new THREE.TrackballControls(camera);
-// controls.target.x += camera.position.x;
 
 // Renderer
 var renderer = new THREE.WebGLRenderer({
@@ -20,14 +22,16 @@ var renderer = new THREE.WebGLRenderer({
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0xfffffff);
 document.body.appendChild(renderer.domElement);
+window.addEventListener( 'resize', onWindowResize, false );
 
-MAX_POINTS = 10000;
-var startingPoint = new THREE.Vector3(0,0,0);
-var startingFace = new THREE.Face3(0,0,0);
+ROWS = 10;
+NUM_POINTS_AND_FACES = 1000;
+console.log(NUM_POINTS_AND_FACES/(ROWS * 2), "rows will be produced.")
+
 var geo = new THREE.Geometry();
-for(i=0; i < MAX_POINTS; i++){
-  geo.vertices.push(startingPoint.clone());
-  geo.faces.push(startingFace.clone());
+for(i=0; i < NUM_POINTS_AND_FACES; i++){
+  geo.vertices.push(new THREE.Vector3(0,0,0));
+  geo.faces.push(new THREE.Face3(0,0,0));
 }
 
 var mesh = new THREE.Mesh(
@@ -35,71 +39,86 @@ var mesh = new THREE.Mesh(
     new THREE.MeshNormalMaterial()
     );
 
-mesh.position.y += 10
 mesh.frustumCulled = false;
-
-mesh.geometry.computeFaceNormals();
-mesh.geometry.computeVertexNormals();
 
 scene.add(mesh);
 
-window.addEventListener("click",onClick);
-
 var currentRow = 0;
-var rows = 150;
 var size = 10;
+var maxHeight = 40
 
-camera.position.x = (rows * size) / 2;
-camera.position.z = (rows * size) / 2;
+mesh.position.x -= ROWS*size/2;
+mesh.position.y -= maxHeight;
 
-function onClick(e){
-  if(currentRow == 0){
+var augmentGeometry = function(){
+ if(currentRow == 0){
     // create initial points - c for column
-    for(var c=0; c < rows; c++){
+    for(var c=0; c < ROWS+1; c++){
       geo.vertices[c] = new THREE.Vector3(c * size, 0, 0);
     }
     currentRow++;
   }
   else{
     // Create points
-    for(var c=0; c < rows+1; c++){
-      mesh.geometry.vertices[currentRow * (rows+1) + c] = new THREE.Vector3(
-          size*c,
-          currentRow*size,
-          Math.random() * 40);
+    for(var c=0; c < ROWS+1; c++){
+      var initialIndex = (ROWS+1) * currentRow + c;
+      mesh.geometry.vertices[initialIndex] = new THREE.Vector3(
+          (size*c),
+          Math.random() * maxHeight,
+          currentRow*size);
     }
     // Create faces
-    for(var c=0; c < rows; c++){
-      mesh.geometry.faces[2*rows*currentRow+(2*c)] = new THREE.Face3(
-          (currentRow*(rows+1)) + c,
-          ((currentRow-1) * (rows+1) + c),
-          (currentRow*(rows+1)) + c + 1);
-      mesh.geometry.faces[2*rows*currentRow+(2*c)+1] = new THREE.Face3(
-          ((currentRow-1) * (rows+1) + c), //0
-          ((currentRow-1) * (rows+1) + c + 1), //1
-          (currentRow*(rows+1)) + c + 1 //8
+    for(var c=0; c < ROWS; c++){
+      var upperLeft = (currentRow*(ROWS+1)) + c + 1; //8
+      var upperRight = (currentRow*(ROWS+1)) + c; // 7
+      var bottomLeft = ((currentRow-1) * (ROWS+1) + c + 1); //1
+      var bottomRight = ((currentRow-1) * (ROWS+1) + c); // 0
+
+      mesh.geometry.faces[(2*ROWS*(currentRow-1)+(2*c))%NUM_POINTS_AND_FACES] = new THREE.Face3(
+          upperLeft,
+          bottomLeft,
+          upperRight
+          );
+      mesh.geometry.faces[(2*ROWS*(currentRow-1)+(2*c)+1)%NUM_POINTS_AND_FACES] = new THREE.Face3(
+          bottomLeft,
+          bottomRight,
+          upperRight
         );
     }
     mesh.geometry.verticesNeedUpdate = true;
     mesh.geometry.elementsNeedUpdate = true;
     mesh.geometry.normalsNeedUpdate = true;
     mesh.geometry.computeFaceNormals();
-    mesh.geometry.computeVertexNormals();
+    // mesh.geometry.computeVertexNormals();
     currentRow++;
 
-    mesh.position.y -= 10;
-  }
+    mesh.position.z -= 10;
+  } 
+}
+
+var onWindowResize = function() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  controls.handleResize();
+  render();
 }
 
 // Render and animate
 var render = function(){
-    // controls.update();
     renderer.render(scene,camera);
 }
 
 var animate = function(){
-    //setTimeout(animate,1000/1);
     requestAnimationFrame( animate );
+    controls.update();
     render();
 }
+
+var geoTimeout = function(){
+  setTimeout(geoTimeout,1000/10);
+  augmentGeometry();
+}
+
 animate();
+geoTimeout();
